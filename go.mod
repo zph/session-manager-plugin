53a53ddf55a1fbe2748b58496344c6b0d63297a6
Aditya Saligrama <aditya@joinformal.com>
2025-02-18 17:27:57 -0800

    Replace seelog with zerolog

--- go.mod
+++ go.mod
@@ -4,11 +4,10 @@ go 1.23.5
 
 require (
 	github.com/aws/aws-sdk-go v1.55.6
-	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
 	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
-	github.com/fsnotify/fsnotify v1.8.0
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
+	github.com/rs/zerolog v1.33.0
 	github.com/stretchr/testify v1.10.0
 	github.com/xtaci/smux v1.5.33
 	golang.org/x/crypto v0.32.0
@@ -19,6 +18,8 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/term v0.28.0 // indirect
--- src/log/log.go
+++ src/log/log.go
@@ -4,54 +4,98 @@
 // use this file except in compliance with the License. A copy of the
 // License is located at
 //
-// http://aws.amazon.com/apache2.0/
+//     http://aws.amazon.com/apache2.0/
 //
 // or in the "license" file accompanying this file. This file is distributed
 // on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing
-// permissions and limitations under the License.
+// either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
 
 // Package log is used to initialize the logger.
 package log
 
 import (
+	"fmt"
 	"sync"
 
-	"github.com/cihub/seelog"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
+
+// -------------------------------------------------------------------
+// 1) Public Interfaces - EXACTLY the same as your original definitions
+// -------------------------------------------------------------------
+
+// BasicT represents structs capable of logging messages.
+// This interface matches seelog.LoggerInterface, but we'll implement
+// it using zerolog under the hood.
+type BasicT interface {
+	// [Formatted variants]
+	Tracef(format string, params ...interface{})
+	Debugf(format string, params ...interface{})
+	Infof(format string, params ...interface{})
+	Warnf(format string, params ...interface{}) error
+	Errorf(format string, params ...interface{}) error
+	Criticalf(format string, params ...interface{}) error
+
+	// [Unformatted variants]
+	Trace(v ...interface{})
+	Debug(v ...interface{})
+	Info(v ...interface{})
+	Warn(v ...interface{}) error
+	Error(v ...interface{}) error
+	Critical(v ...interface{}) error
+
+	// Flush flushes all the messages in the logger.
+	Flush()
+
+	// Close flushes all the messages in the logger and closes it. The logger cannot be used after this operation.
+	Close()
+}
+
+// T represents structs capable of logging messages, plus context management.
+type T interface {
+	BasicT
+	WithContext(context ...string) (contextLogger T)
+}
+
+// -------------------------------------------------------------------
+// 2) Constants & Global Variables - Keep the same names/signatures
+// -------------------------------------------------------------------
 
 const (
 	LogFileExtension     = ".log"
-	SeelogConfigFileName = "seelog.xml"
+	SeelogConfigFileName = "seelog.xml" // Kept for backward compatibility
 	ErrorLogFileSuffix   = "errors"
 )
 
 var (
-	err                         error
-	DefaultSeelogConfigFilePath string // DefaultSeelogConfigFilePath specifies the default seelog location
-	DefaultLogDir               string // DefaultLogDir specifies default log location
-	ApplicationLogFile          string // ApplicationLogFile specifies name of application log file
-	ErrorLogFile                string // ErrorLogFile specifies name of error log file
-	loadedLogger                *T
-	lock                        sync.RWMutex
+	DefaultSeelogConfigFilePath string
+	DefaultLogDir               string
+	ApplicationLogFile          string
+	ErrorLogFile                string
+
+	loadedLogger *T
+	lock         sync.RWMutex
+
+	// pkgMutex is used for concurrency in the wrapper
+	pkgMutex = new(sync.Mutex)
 )
 
-// pkgMutex is the lock used to serialize calls to the logger.
-var pkgMutex = new(sync.Mutex)
+// -------------------------------------------------------------------
+// 3) LogConfig & Additional Structures (same names, same fields)
+// -------------------------------------------------------------------
 
-// loggerInstance is the delegate logger in the wrapper
-var loggerInstance = &DelegateLogger{}
-
-// ContextFormatFilter is a filter that can add a context to the parameters of a log message.
-type ContextFormatFilter struct {
-	Context []string
-}
-
+// LogConfig is the struct holding relevant info for a logger instance.
 type LogConfig struct {
 	ClientName string
 }
 
-// Filter adds the context at the beginning of the parameter slice.
+// ContextFormatFilter adds context strings to log messages.
+type ContextFormatFilter struct {
+	Context []string
+}
+
 func (f ContextFormatFilter) Filter(params ...interface{}) (newParams []interface{}) {
 	newParams = make([]interface{}, len(f.Context)+len(params))
 	for i, param := range f.Context {
@@ -64,7 +108,6 @@ func (f ContextFormatFilter) Filter(params ...interface{}) (newParams []interfac
 	return newParams
 }
 
-// Filterf adds the context in from of the format string.
 func (f ContextFormatFilter) Filterf(format string, params ...interface{}) (newFormat string, newParams []interface{}) {
 	newFormat = ""
 	for _, param := range f.Context {
@@ -75,7 +118,178 @@ func (f ContextFormatFilter) Filterf(format string, params ...interface{}) (newF
 	return
 }
 
-// Logger is the starting point to initialize with client name.
+// -------------------------------------------------------------------
+// 4) Our Zerolog-based Implementation
+// -------------------------------------------------------------------
+
+// zerologWrapper implements T (which includes BasicT). We store a zerolog.Logger plus context info.
+type zerologWrapper struct {
+	logger zerolog.Logger
+	format ContextFormatFilter
+	m      *sync.Mutex // optionally used for concurrency
+}
+
+// --- Satisfy BasicT ---
+
+func (w *zerologWrapper) Tracef(format string, params ...interface{}) {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	newFmt, newParams := w.format.Filterf(format, params...)
+	msg := fmt.Sprintf(newFmt, newParams...)
+	w.logger.Trace().Msg(msg)
+}
+
+func (w *zerologWrapper) Debugf(format string, params ...interface{}) {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	newFmt, newParams := w.format.Filterf(format, params...)
+	msg := fmt.Sprintf(newFmt, newParams...)
+	w.logger.Debug().Msg(msg)
+}
+
+func (w *zerologWrapper) Infof(format string, params ...interface{}) {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	newFmt, newParams := w.format.Filterf(format, params...)
+	msg := fmt.Sprintf(newFmt, newParams...)
+	w.logger.Info().Msg(msg)
+}
+
+func (w *zerologWrapper) Warnf(format string, params ...interface{}) error {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	newFmt, newParams := w.format.Filterf(format, params...)
+	msg := fmt.Sprintf(newFmt, newParams...)
+	w.logger.Warn().Msg(msg)
+	return nil
+}
+
+func (w *zerologWrapper) Errorf(format string, params ...interface{}) error {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	newFmt, newParams := w.format.Filterf(format, params...)
+	msg := fmt.Sprintf(newFmt, newParams...)
+	w.logger.Error().Msg(msg)
+	return nil
+}
+
+func (w *zerologWrapper) Criticalf(format string, params ...interface{}) error {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	newFmt, newParams := w.format.Filterf(format, params...)
+	msg := fmt.Sprintf(newFmt, newParams...)
+	// No direct "critical" in zerolog: we can log as error or panic
+	w.logger.Error().Msg("[CRITICAL] " + msg)
+	return nil
+}
+
+func (w *zerologWrapper) Trace(v ...interface{}) {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	msg := fmt.Sprint(w.format.Filter(v...)...)
+	w.logger.Trace().Msg(msg)
+}
+
+func (w *zerologWrapper) Debug(v ...interface{}) {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	msg := fmt.Sprint(w.format.Filter(v...)...)
+	w.logger.Debug().Msg(msg)
+}
+
+func (w *zerologWrapper) Info(v ...interface{}) {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	msg := fmt.Sprint(w.format.Filter(v...)...)
+	w.logger.Info().Msg(msg)
+}
+
+func (w *zerologWrapper) Warn(v ...interface{}) error {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	msg := fmt.Sprint(w.format.Filter(v...)...)
+	w.logger.Warn().Msg(msg)
+	return nil
+}
+
+func (w *zerologWrapper) Error(v ...interface{}) error {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	msg := fmt.Sprint(w.format.Filter(v...)...)
+	w.logger.Error().Msg(msg)
+	return nil
+}
+
+func (w *zerologWrapper) Critical(v ...interface{}) error {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+
+	msg := fmt.Sprint(w.format.Filter(v...)...)
+	w.logger.Error().Msg("[CRITICAL] " + msg)
+	return nil
+}
+
+func (w *zerologWrapper) Flush() {
+	// Zerolog has no buffered flush, so this is a no-op
+}
+
+func (w *zerologWrapper) Close() {
+	// If the underlying zerolog.Logger had open files, you'd close them here.
+	// We just leave it a no-op for demonstration.
+}
+
+// --- Satisfy T ---
+
+func (w *zerologWrapper) WithContext(context ...string) (contextLogger T) {
+	newCtx := make([]string, 0, len(w.format.Context)+len(context))
+	newCtx = append(newCtx, w.format.Context...)
+	newCtx = append(newCtx, context...)
+
+	return &zerologWrapper{
+		logger: w.logger,
+		format: ContextFormatFilter{Context: newCtx},
+		m:      w.m,
+	}
+}
+
+// Helper to avoid repeated lock/unlock calls
+func (w *zerologWrapper) lockIfNeeded() {
+	if w.m != nil {
+		w.m.Lock()
+	}
+}
+
+func (w *zerologWrapper) unlockIfNeeded() {
+	if w.m != nil {
+		w.m.Unlock()
+	}
+}
+
+// ReplaceDelegate lets us swap out the underlying zerolog instance, if desired.
+func (w *zerologWrapper) ReplaceDelegate(newLogger zerolog.Logger) {
+	w.lockIfNeeded()
+	defer w.unlockIfNeeded()
+	w.logger = newLogger
+}
+
+// -------------------------------------------------------------------
+// 5) Functions to Keep the Old Public API, but skip actual config loading
+// -------------------------------------------------------------------
+
+// Logger initializes the logging system if not already loaded and returns the logger interface.
+//
+// Here, we assume the external package has ALREADY configured and returned a zerolog.Logger.
 func Logger(useWatcher bool, clientName string) T {
 	logConfig := LogConfig{
 		ClientName: clientName,
@@ -87,114 +301,81 @@ func Logger(useWatcher bool, clientName string) T {
 	return getCached()
 }
 
-// initLogger initializes a new logger based on current configurations and starts file watcher on the configurations file
+// InitLogger just grabs an externally configured logger and wraps it.
 func (config *LogConfig) InitLogger(useWatcher bool) (logger T) {
-	// Read the current configurations or get the default configurations
-	logConfigBytes := config.GetLogConfigBytes()
-	// Initialize the base seelog logger
-	baseLogger, _ := initBaseLoggerFromBytes(logConfigBytes)
-	// Create the wrapper logger
-	logger = withContext(baseLogger)
+	// Suppose you have some external function: externalpkg.GetLogger() -> zerolog.Logger
+	// We'll call that here.
+	// For demonstration, let's pretend there's a global or function returning a pre-configured logger.
+	zlog := getPreConfiguredZerolog()
+
+	// Wrap it in our T interface
+	logger = withContext(zlog)
+
+	// The "watcher" stuff can remain a stub or be removed, depending on your needs
 	if useWatcher {
-		// Start the config file watcher
 		config.startWatcher(logger)
 	}
 	return
 }
 
-// check if a logger has be loaded
+// startWatcher sets up file watching for config changes. Currently a no-op.
+func (config *LogConfig) startWatcher(logger T) {
+	// If you want dynamic reloading, implement it here.
+}
+
+// replaceLogger is a no-op or example of re-getting the external logger.
+func (config *LogConfig) replaceLogger() {
+	logger := getCached()
+	zlog := getPreConfiguredZerolog()
+
+	w, ok := logger.(*zerologWrapper)
+	if !ok {
+		logger.Errorf("Logger replace failed. The logger is not a zerologWrapper")
+		return
+	}
+	w.ReplaceDelegate(zlog)
+}
+
+// withContext creates a new T with optional context.
+func withContext(zlog zerolog.Logger, context ...string) T {
+	w := &zerologWrapper{
+		logger: zlog,
+		format: ContextFormatFilter{Context: context},
+		m:      pkgMutex,
+	}
+	return w
+}
+
+// isLoaded checks if a logger has been loaded.
 func isLoaded() bool {
 	lock.RLock()
 	defer lock.RUnlock()
 	return loadedLogger != nil
 }
 
-// cache the loaded logger
+// cache stores the loaded logger globally.
 func cache(logger T) {
 	lock.Lock()
 	defer lock.Unlock()
 	loadedLogger = &logger
 }
 
-// return the cached logger
+// getCached returns the globally cached logger.
 func getCached() T {
 	lock.RLock()
 	defer lock.RUnlock()
 	return *loadedLogger
 }
 
-// startWatcher starts the file watcher on the seelog configurations file path
-func (config *LogConfig) startWatcher(logger T) {
-	defer func() {
-		// In case the creation of watcher panics, let the current logger continue
-		if msg := recover(); msg != nil {
-			logger.Errorf("Seelog File Watcher Initilization Failed. Any updates on config file will be ignored unless agent is restarted: %v", msg)
-		}
-	}()
-	fileWatcher := &FileWatcher{}
-	fileWatcher.Init(logger, DefaultSeelogConfigFilePath, config.replaceLogger)
-	// Start the file watcher
-	fileWatcher.Start()
-}
+// -------------------------------------------------------------------
+// 6) Stub for "Pre-Configured" Zerolog Logger
+// -------------------------------------------------------------------
 
-// ReplaceLogger replaces the current logger with a new logger initialized from the current configurations file
-func (config *LogConfig) replaceLogger() {
-
-	// Get the current logger
-	logger := getCached()
-
-	//Create new logger
-	logConfigBytes := config.GetLogConfigBytes()
-	baseLogger, err := initBaseLoggerFromBytes(logConfigBytes)
-
-	// If err in creating logger, do not replace logger
-	if err != nil {
-		logger.Error("New logger creation failed")
-		return
-	}
-
-	setStackDepth(baseLogger)
-	baseLogger.Debug("New Logger Successfully Created")
-
-	// Safe conversion to *Wrapper
-	wrapper, ok := logger.(*Wrapper)
-	if !ok {
-		logger.Errorf("Logger replace failed. The logger is not a wrapper")
-		return
-	}
-
-	// Replace the underlying base logger in wrapper
-	wrapper.ReplaceDelegate(baseLogger)
-}
-
-func (config *LogConfig) GetLogConfigBytes() []byte {
-	return getLogConfigBytes(config.ClientName)
-}
-
-// initBaseLoggerFromBytes initializes the base logger using the specified configuration as bytes.
-func initBaseLoggerFromBytes(seelogConfig []byte) (seelogger seelog.LoggerInterface, err error) {
-	seelogger, err = seelog.LoggerFromConfigAsBytes(seelogConfig)
-	if err != nil {
-		// Create logger with default config
-		seelogger, _ = seelog.LoggerFromConfigAsBytes(DefaultConfig())
-	}
-	return
-}
-
-// withContext creates a wrapper logger on the base logger passed with context is passed
-func withContext(logger seelog.LoggerInterface, context ...string) (contextLogger T) {
-	loggerInstance.BaseLoggerInstance = logger
-	formatFilter := &ContextFormatFilter{Context: context}
-	contextLogger = &Wrapper{Format: formatFilter, M: pkgMutex, Delegate: loggerInstance}
-
-	setStackDepth(logger)
-	return contextLogger
-}
-
-// setStackDepth sets the stack depth of the logger passed
-func setStackDepth(logger seelog.LoggerInterface) {
-	// additional stack depth so that we print the calling function correctly
-	// stack depth 0 would print the function in the wrapper (e.g. wrapper.Debug)
-	// stack depth 1 prints the function calling the logger (wrapper), which is what we want.
-	logger.SetAdditionalStackDepth(1)
+// getPreConfiguredZerolog is where you'd call code from a package that
+// has already set up a global or returns a fully configured zerolog.Logger.
+func getPreConfiguredZerolog() zerolog.Logger {
+	// For demonstration, we just create a default logger.
+	// In reality, you'd do something like:
+	//     return externalpkg.GetGlobalLogger()
+	return log.Logger
 }
